47283bf9e79eaa4cfa74c668d9744adc55a9f496
Matthew Drumheller <MattDrummy@gmail.com>
2017-10-07 08:47:29 -0600

    fixed bug with path

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const WebSocketServer = require('ws').Server;
 const WebSocket = require('ws')
 const rooom1 = new WebSocketServer({
   port: 'https://thawing-stream-29526.herokuapp.com/',
-  path: 'room1'
+  path: 'room1',
   verifyClient: function(info){
     return true
   }
 });
 const room2 = new WebSocketServer({
   server: 'https://thawing-stream-29526.herokuapp.com/',
-  path: 'room2'
+  path: 'room2',
   verifyClient: function(info){
     return true;
   }
